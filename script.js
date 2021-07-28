5ca58dfe19a4ad7c0695288a8284f8a7f4c9ffe3
Lai <laile110395@gmail.com>
2021-07-27 23:14:22 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log ("testing!");
+
+var like = [9,12,9]
+var spans = [
+    document.querySelector("post1"),
+    document.querySelector("post2"),
+    document.querySelector("post3"),
+];
+
+function likes(id){
+    likes[id]++;
+    spans[id].innerHTML = like[id] + like(s); 
+}
